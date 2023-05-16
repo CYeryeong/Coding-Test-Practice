6acbcf6620067c3f714cf1e0510e7465c29576c5
CYeryeong <joyena2001@naver.com>
2023-05-16 17:51:28 +0900

    answer ver

--- level2_근무시간.py
+++ level2_근무시간.py
@@ -7,29 +7,25 @@ Thur_in, Thur_out = input().split()
 Fri_in, Fri_out = input().split()
 
 Time_list = [Mon_in, Mon_out, Tue_in, Tue_out, Wen_in, Wen_out, Thur_in, Thur_out, Fri_in, Fri_out]
-Time = []
-Work_Time = []
+Time, Work_Time_Hours, Work_Time_Min = [], [], []
+result= 0 
+
 for i in Time_list :
-    Time.extend(i.split(':'))
+    Time.extend(map(int, i.split(':')))
 
-print(Time)
-for i in range(0, 11) :
-    if i%2 == 0 and int(Time[i+2]) - int(Time[i]) > 0:
-        Work_Time.append(int(Time[i+2]) - int(Time[i]))
-    elif i%2 == 0 and int(Time[i+2]) - int(Time[i]) < 0:
-        Work_Time.append(- int(Time[i+2]) + int(Time[i]))
-    elif i%2 != 0 and int(Time[i+2]) - int(Time[i]) < 0 :
-        Work_Time.append(60 + Time[i+2]) - int(Time[i])
-    else :
-        Work_Time.append(int(Time[i+2]) - int(Time[i]))
-print(Work_Time)
-result = 0
+# Time = [10, 0, 19, 0, 9, 0, 15, 0, 10, 0, 11, 0, 11, 0, 22, 0, 9, 0, 15, 0]
+# 2-0 = Mon_hour, 3-1 = Mon_min => 4를 한 턴으로
+# hour은 음수일리 없음, min의 경우 음수이면 +60 하면 됨 그럴 경우에는 hour-60도 필요
+for i in range(len(Time)//4):
+    Work_Time_Hours.append(60*(Time[i*4+2] - Time[i*4]))
+    if Time[i*4+3] - Time[i*4+1] < 0:
+        Work_Time_Min.append(60+Time[i*4+3] - Time[i*4+1])
+        Work_Time_Hours[i] -= 60
+    else:
+        Work_Time_Min.append(Time[i*4+3] - Time[i*4+1])
 
-for i in range(len(Work_Time)):
-    if i%4 == 0:
-        result = result + 60*Work_Time[i]
-        print(result)
-    else :
-        result = result + Work_Time[i]
-        print(result)
+total = Work_Time_Hours + Work_Time_Min
+for i in range(len(total)):
+    result += total[i]
 
+print(result)
