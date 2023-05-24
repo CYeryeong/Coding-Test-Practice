6c96a07e10dc640b44390b00ec0772ceff6fd8e8
CYeryeong <joyena2001@naver.com>
2023-05-24 19:55:51 +0900

    self_1

---
+++ level2_금고털이.py
@@ -0,0 +1,19 @@
+import sys
+
+W, n = map(int, input().split())
+total_price = 0
+Weight, price = [],[]
+
+#lst 완성성
+for _ in range(n):
+    Wei, pri = map(int, input().split())
+    Weight.append(Wei)
+    price.append(pri)
+
+for i in range
+
+
+# W보다 작으면서 n이 가장 높은 것을 먼저 담는다.
+
+
+
