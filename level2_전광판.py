c1b30e0e06de296831211518ec03c19704c3e4ba
CYeryeong <joyena2001@naver.com>
2023-05-24 19:41:19 +0900

    solution_1

--- level2_전광판.py
+++ level2_전광판.py
@@ -1,52 +1,40 @@
 import sys
-# 1 / 2 3 / 4 / 5 6 / 7 위에서부터
-n_0 = [1, 1, 1, 0, 1, 1, 1]
-n_1 = [0, 0, 1, 0, 0, 1, 0]
-n_2 = [1, 0, 1, 1, 1, 0, 1]
-n_3 = [1, 0, 1, 1, 0, 1, 1]
-n_4 = [0, 1, 1, 1, 0, 1, 0]
-n_5 = [1, 1, 0, 1, 0, 1, 1]
-n_6 = [1, 1, 0, 1, 1, 1, 1]
-n_7 = [1, 1, 1, 0, 0, 1, 0]
-n_8 = [1, 1, 1, 1, 1, 1, 1]
-n_9 = [1, 1, 1, 1, 0, 1, 1]
-n_10n = [0, 0, 0, 0, 0, 0, 0]
-n_list = [n_0, n_1, n_2, n_3, n_4, n_5, n_6, n_7, n_8, n_9, n_10n]
-n = int(input())
+input = sys.stdin.readline 
+
+# 정보 저장
+# 각 전구의 순서를 정하고, 0과 1로 구분
+
+info = {
+    
+    '0' : '1110111',
+    '1' : '0010010',
+    '2' : '1011101',
+    '3' : '1011011',
+    '4' : '0111010', 
+    '5' : '1101011',
+    '6' : '1101111',
+    '7' : '1110010',
+    '8' : '1111111',
+    '9' : '1111011',
+    ' ' : '0000000'
+}
 
 
-for i in range(0,n):
-    total = 0
-    n1, n2= map(int, input().split())
-    n1 = str('{0:05d}'.format(n1))
-    n2 = str('{0:05d}'.format(n2))
-    for z in (0,5):
-        if n2[0:z+1] != 0*(z+1):
-            for j in range(0,5):
-                if n1[j] != n2[j]:
-                    n1_num = n_list[int(n1[j])]
-                    n2_num = n_list[int(n2[j])]
-                    print(n1_num, n2_num)
-                    for k in range(0,7):
-                        if n1_num[k] != n2_num[k]:
-                            total += 1
-                        else :
-                            continue
-                else:
-                    continue
-        else : 
-            n1_num = n_list[int(n1[j])]
-            n2_num = n_list[10]
-            for j in range(0,5):
-                if n1[j] != n2[j]:
-                    n1_num = n_list[int(n1[j])]
-                    n2_num = n_list[int(n2[j])]
-                    print(n1_num, n2_num)
-                    for k in range(0,7):
-                        if n1_num[k] != n2_num[k]:
-                            total += 1
-                        else :
-                            continue
-    print(total)
+t = int(input())
+for _ in range(t):
+    a, b = map(str,input().split())
+    
+    
+    #전처리
+    a_zero, b_zero = 5-len(a), 5-len(b)
+    a = ' ' * a_zero + a
+    b = ' ' * b_zero + b
 
+    result = 0
 
+    for i in range(5):
+        for j in range(7):
+            if(info[a[i]][j] != info[b[i]][j]):
+                result+=1
+                
+    print(result)
