fa93d36f676326a7796c795cb61442fb27252cfd
CYeryeong <joyena2001@naver.com>
2023-05-26 14:52:07 +0900

    answer ver

--- level2_8단 변속기py.py
+++ level2_8단 변속기py.py
@@ -1,18 +1,15 @@
 import sys
-input_lst = list(map(int, sys.stdin.readline().split()))
-# 이게 중요하다!! 메모리를 많이 차지ㅇ하지 않는다
-result = 0
 
-for i in range(7):
-    if input_lst[i+1] == input_lst[i]+1:
-        result +=1
-    elif input_lst[i+1] == input_lst[i]-1:
-        result -= 1
+number_list = list(map(int, sys.stdin.readline().split())) # 숫자를 리스트화 시켜서 비교하기 쉽게 만듬
 
-if result == 7:
-    print('ascending')
-elif result == -7 :
-    print('descending')
-else:
-    print('mixed')
+if number_list == [1,2,3,4,5,6,7,8]: # 1부터 오름차순이면
+    print("ascending")
+
+elif number_list == [8,7,6,5,4,3,2,1]: # 8부터 내림차순이면
+    print("descending")
+
+else: # 그 외
+    print("mixed")
+    
+#개똑똑하다미친,,,,, 그냥 이렇게 리스트로 넣어버리면 되는군
 
