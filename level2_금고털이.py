bb4f09074c7922cd491cb2841d1003a530296fc1
CYeryeong <joyena2001@naver.com>
2023-05-26 14:38:48 +0900

    answer ver

--- level2_금고털이.py
+++ level2_금고털이.py
@@ -1,34 +1,30 @@
 import sys
+ 
+input = sys.stdin.readline
 
-rest_g, n = map(int, input().split())
-total_price = 0
-Weight, price = [],[]
+w, n = map(int, input().split())
+ 
+jewels = [list(map(int, input().split())) for _ in range(n)]
+ 
+jewels.sort(key=lambda x: x[1], reverse=True)
+ 
+answer = 0
+for weight, price in jewels:
+    if w > weight:
+        answer += weight * price
+        w -= weight
+    else:
+        answer += w * price
+        break
+ 
+print(answer)
 
-#lst 완성
-for _ in range(n):
-    Wei, pri = map(int, input().split())
-    Weight.append(Wei)
-    #Weight = [90,70]
-    price.append(pri)
-    #price = [1, 2]
-
-while rest_g > 0 :
-    max_g = Weight[price.index(max(price))]
-    max_p = max(price)
-
-    if max_g > rest_g :
-        total_price += rest_g*max_p
-        rest_g = 0
-
-    else :
-        total_price += max_g*max_p
-        rest_g = rest_g - max_g
-        Weight[price.index(max(price))] = 0
-        price[price.index(max(price))] = 0 
-
-print(total_price)
 
 #자꾸 시간초과된다. 왜?어떻게 고쳐야할까
-
+#보석을 가방에 최대한 담되 가장 비싼 보석들을 위주로 가져가는 그리디 알고리즘 문제입니다.
+#무게당 가격이 비싼 보석 순으로 가져가면 되며, 무게가 부족하면 톱으로 귀금속을 잘라서 
+# 가져갈 수 있으므로 넣을 수 있는 무게만큼 최대한 가져가면 됩니다. 
+# 보석을 가격순으로 정렬 후 반복하면서 (무게 * 무게 당 가격)만큼 answer를 증가시키고 
+# 무게가 모자라면 (남은 공간 * 해당 보석의 무게 당 가격)을 증가시키고 반복을 멈추면 해결할 수 있습니다.
 
 
